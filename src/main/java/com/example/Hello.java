5833e9c9a91dd454df276a8af978dfe471ed6b00
MengZhang <meng.zhang15@ufl.edu>
2017-04-25 14:06:51 -0400

    update hello partern

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,6 +2,7 @@ package com.example;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -10,11 +11,11 @@ import javax.ws.rs.core.MediaType;
  * @author mike
  */
 
-@Path("hello")
+@Path("hello/{username}")
 public class Hello {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String test() {
-        return "Hello, Heroku from mike!";
+    public String test(@PathParam("username") String userName) {
+        return "Hello, Heroku from " + userName + "!";
     }
 }
