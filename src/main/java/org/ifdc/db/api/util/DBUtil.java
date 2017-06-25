661f86797e59974ad01d5da0d596e061213ecd0a
MengZhang <meng.zhang15@ufl.edu>
2017-06-25 15:40:06 -0400

    modify for compile on PC

--- src/main/java/org/ifdc/db/api/util/DBUtil.java
+++ src/main/java/org/ifdc/db/api/util/DBUtil.java
@@ -9,8 +9,8 @@ import com.mongodb.MongoClientURI;
 public class DBUtil {
     
     public static MongoClientURI getDBURI() {
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb://mikecomic:Mike0105@cluster0-shard-00-00-upixo.mongodb.net:27017,cluster0-shard-00-01-upixo.mongodb.net:27017,cluster0-shard-00-02-upixo.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+        String dbPath = ""; // Give your DB path here
+        MongoClientURI uri = new MongoClientURI(dbPath);
         return uri;
     }
     
